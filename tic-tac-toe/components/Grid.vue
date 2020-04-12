9e0171249b88dd2f64bac37066a4de6f9774fe08
avimehenwal <avi.mehanwal@gmail.com>
2020-04-12 20:20:07 +0200

    Add game grid component

---
+++ tic-tac-toe/components/Grid.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card>
+    <v-card-title>Tic Tac Toe</v-card-title>
+    {{ num }}
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  props: {
+    num: {
+      type: Number,
+      default: 9
+    },
+  }
+}
+</script>
--- tic-tac-toe/pages/inspire.vue
+++ tic-tac-toe/pages/inspire.vue
@@ -1,19 +1,13 @@
 <template>
-  <v-layout>
-    <v-flex class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
-  </v-layout>
+  <Grid num="9"/>
 </template>
+
+<script>
+import Grid from '@/components/Grid.vue'
+
+export default {
+  components: {
+    Grid
+  }
+}
+</script>
